c394168c7ed0550f0506c4fcdc354e56487bdef4
Silviya Deyanova <sjdejanova@uni-sofia.bg>
2023-01-06 17:23:42 +0200

    changed placeholder style of elemets

--- src/app/components/test-gridstack/test-gridstack.component.ts
+++ src/app/components/test-gridstack/test-gridstack.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { GridStack } from 'gridstack';
 
 @Component({
@@ -8,13 +8,29 @@ import { GridStack } from 'gridstack';
 })
 export class TestGridstackComponent implements OnInit {
   constructor() {}
+  public options = {
+    column: 12,
+    resizable: {
+      handles: 'e,w', //can specify 'e,se,s,sw,w' for direction in which to resize
+    },
+    minRow: 1,
+  };
 
   ngOnInit(): void {
-    GridStack.init({
-      column: 12,
-      resizable: {
-        handles: 'e,w', //can specify 'e,se,s,sw,w' for direction in which to resize
-      },
-    });
+    GridStack.init(this.options);
+
+    // GridStack.setupDragIn('.sidebar .grid-stack-item', {
+    //   appendTo: 'body',
+    //   helper: (event: any) => {
+    //     return event.target.cloneNode(true);
+    //   },
+    // });
   }
+
+  // @HostListener('document:click', ['$event'])
+  // drag(event: any) {}
+
+  // handleDrop(event: any): void {
+  //   return event.target.cloneNode(true);
+  // }
 }
